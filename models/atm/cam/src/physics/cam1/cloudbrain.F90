917287c46598ffb5e4ef454533fb6f84f594aa1e
Michael Pritchard <tg802402@login2.stampede2.tacc.utexas.edu>
2021-05-26 12:28:23 -0500

    Cleaned up non-FKB detritus, first stage (incomplete) of multi-instance FKB array passed compile.

--- models/atm/cam/src/physics/cam1/cloudbrain.F90
+++ models/atm/cam/src/physics/cam1/cloudbrain.F90
@@ -1,7 +1,7 @@
 #include <misc.h>
 #include <params.h>
-!#define BRAINDEBUG
-
+#define BRAINDEBUG
+#define NEURALLIB
 module cloudbrain
 use shr_kind_mod,    only: r8 => shr_kind_r8
 use ppgrid,          only: pcols, pver, pverp
@@ -22,33 +22,19 @@ use mod_ensemble, only: ensemble_type
 
   private
   ! Define variables for this entire module
-  integer, parameter :: nn_nint = 8
   integer, parameter :: inputlength = 94
   integer, parameter :: outputlength = 65
-  integer, parameter :: activation_type = 1
-  integer, parameter :: width = 256
 
 #ifdef NEURALLIB
-#ifdef ENSEMBLE
-  real(rk) :: noise = 0.0
-  type(ensemble_type) :: cloudbrain_ensemble
-#else
-  type(network_type) :: cloudbrain_net
+!  type(network_type) :: cloudbrain_net
+  type(network_type) :: cloudbrain_net(65)
   integer(ik) :: fileunit, num_layers
   integer(ik) :: n
-#endif
-#else
-  real :: weights_inp(width, inputlength)
-  real :: bias_inp(width)
-  real :: weights_int(nn_nint, width, width)
-  real :: bias_int(nn_nint, width)
-  real :: weights_out(outputlength, width)
-  real :: bias_out(outputlength)
 #endif
 
   real :: inp_sub(inputlength)
   real :: inp_div(inputlength)
-  real :: out_scale(outputlength)
+  real :: out_scale(outputlength) ! HEY needs updating.
 
   public neural_net, init_keras_matrices, init_keras_norm
   contains
@@ -76,22 +62,19 @@ use mod_ensemble, only: ensemble_type
     real(r8), intent(out) :: PRECT
     ! Allocate utilities
 #ifdef NEURALLIB
-    real(rk) :: input(inputlength),x1(width), x2(width)
-#else
-    real(r8) :: input(inputlength),x1(width), x2(width)
+    real(rk) :: input(inputlength)
 #endif
     real(r8) :: output (outputlength)
-    integer :: k, nlev, n
+    integer :: k, nlev, n,count
     integer, intent(in) :: icol
 
     ! 1. Concatenate input vector to neural network
     nlev=30
+! HEY we are not sure of the input order
+! HEY the input normalization files look suspicious.
 #ifdef NEURALLIB
-    input(1:nlev) = TBP(:) !QBP(:) 
-    input((nlev+1):2*nlev) = QBP(:) !TBP(:)
-#else
-    input(1:nlev) = QBP(:)
-    input((nlev+1):2*nlev) = TBP(:)
+    input(1:nlev) = TBP(:)
+    input((nlev+1):2*nlev) = QBP(:)
 #endif
     input((2*nlev+1):3*nlev)=VBP(:)
     input(3*nlev+1) = PS
@@ -116,35 +99,10 @@ use mod_ensemble, only: ensemble_type
 
 ! 3. Neural network matrix multiplications and activations
 #ifdef NEURALLIB
-#ifdef ENSEMBLE
-    output = cloudbrain_ensemble % average(input)
-#else
     ! use neural fortran library
-    output = cloudbrain_net % output(input)
-#endif
-#else
-    ! hard code layers
-    ! 3.1 1st layer: input length-->256.
-    call matmul(input, x1, inputlength, width, weights_inp, bias_inp, activation_type)
-#ifdef BRAINDEBUG
-        if (masterproc .and. icol .eq. 1) then
-          write (6,*) 'BRAINDEBUG layer = ',1
-          write (6,*) 'BRAINDEBUG output = ',x1
-        endif
-#endif
-    ! 3.2 Intermediate layers
-    do n=1,nn_nint
-      call matmul(x1, x2, width, width, weights_int(n, :, :), bias_int(n, :), activation_type)
-      x1(:) = x2(:)
-#ifdef BRAINDEBUG
-        if (masterproc .and. icol .eq. 1) then
-          write (6,*) 'BRAINDEBUG layer = ',n+1
-          write (6,*) 'BRAINDEBUG output = ',x1
-        endif
-#endif
-    end do
-    ! 3.3 Last layer with linear activation
-    call matmul(x1, output, width, outputlength, weights_out, bias_out, 0)
+   do k=1,outputlength
+     output = cloudbrain_net(k) % output(input) ! note coupling to many NNs here, one for each output
+   end do
 #endif
 
 #ifdef BRAINDEBUG
@@ -180,185 +138,31 @@ use mod_ensemble, only: ensemble_type
 
   end subroutine neural_net
 
-#ifdef NEURALLIB
-#else
-  subroutine matmul(inp, out, len_in, len_out, weights, bias, act_type)
-    ! Also do LeakyReLU in here
-    ! Neural network matrix multiplication
-    ! Activation type: 0 = linear, 1 = LeakyReLU
-    integer, intent(in) :: len_in, len_out, act_type
-    real, intent(in)    :: inp(len_in)
-    real, intent(out)   :: out(len_out)
-    real, intent(in)    :: bias(len_out)
-    real, intent(in)    :: weights(len_out,len_in)
-    integer             :: k, j
-  
-    out(:) = 0.
-    do k=1,len_out
-      do j=1,len_in
-        out(k) = out(k) + weights(k,j)* inp(j)
-      end do
-      out(k) = out(k) + bias(k)
-    end do
-  
-    if (act_type .eq. 1) then
-      call leaky_relu(out, len_out, 0.3)
-    end if
-  
-  end subroutine matmul
-  
-  
-  subroutine leaky_relu(x, len, alpha)
-    real, intent(inout)    :: x(len)
-    integer, intent(in)    :: len
-    real, intent(in)       :: alpha
-    integer                :: k
-    do k=1,len
-      x(k) = max(alpha * x(k), x(k))  ! Leaky ReLU
-    end do
-  end subroutine leaky_relu
-#endif
-
-
   subroutine init_keras_matrices()    
 #ifdef NEURALLIB
-#ifdef ENSEMBLE
-    write (6,*) '------- NEURAL-FORTRAN: ensemble loading -------'
-    cloudbrain_ensemble = ensemble_type('./Models/', noise)
-    write (6,*) '------- NEURAL-FORTRAN: ensemble loaded -------'
-#else
-    call cloudbrain_net % load('./keras_matrices/model.txt')
-    write (6,*) '------- NEURAL-FORTRAN: loaded network from txt file -------'
-#endif
-#else
-#ifdef BRAINDEBUG
-    do n = 1, size(cloudbrain_net % layers)
-        write (6,*) 'BRAINDEBUG layer weight', n, cloudbrain_net % layers(n) % w(1$
-    end do
-#endif
-    ! load weights and bias for each layer
-    integer :: n, k, ios
-    character(len=1) :: str
-    character(len=2) :: str10  ! Yeah, I don't know how to deal with one vs two characters
-    character(len=22) :: pref
-    character(len=9) :: suf_bias
-    character(len=11) :: suf_kernel
+! HEY manage the enumerated filenames here:
+! Upgrade into a loop over all the laods
+ ! LOGIC FOR LOADING ALL THE SEPARATE NNS IS HERE  
+  integer :: k,count
 
-    pref = './keras_matrices/layer'
-    suf_bias = '_bias.txt'
-    suf_kernel = '_kernel.txt'
-
-    ! 1. Input layer
-    if (masterproc) then
-      write (6,*) 'CLOUDBRAIN: reading layer1_bias'
-    endif
-    open (unit=555,file='./keras_matrices/layer1_bias.txt',status='old',action='read',iostat=ios)
-    if (ios .ne. 0) then
-      write (6,*) 'CLOUDBRAIN keras matrices unable to load, abort.'
-      stop
-    endif
-    read(555,*) bias_inp(:)
-    close (555)
-    if (masterproc) then
-      write (6,*) 'CLOUDBRAIN: reading layer1_kernel'
-    endif
-    open (unit=555,file='./keras_matrices/layer1_kernel.txt',status='old',action='read') 
-    do k=1,width
-      read(555,*) weights_inp(k,:)
-    end do
-    close (555)
-#ifdef BRAINDEBUG
-      if (masterproc) then
-        write (6,*) 'BRAINDEBUG weights_inp = ',weights_inp
-        write (6,*) 'BRAINDEBUG bias_inp = ',bias_inp
-      endif
+  count = 0
+  do k=1,65
+      ! HEY insert the fortran string handling here.
+      ! (pending successful txt conversion of the saved models via FKB)
+      call cloudbrain_net(count) % load('/home1/08098/tg873976/usmile/causality_convection/dummy_singleNNs_FKB_renamed/0_15_model.h5')
+      ! YO do arrays of cloiudbrain_net % load operations work? TBD...
+      write (6,*) 'Causal-coupler loaded network ', count
+  end do
 #endif
-
-    ! 2. Intermediate layers
-    do n=1,nn_nint
-      write (str, '(I1)') n+1
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_bias', n+1, pref//trim(str)//suf_bias
-      endif
-      open (unit=555, file=pref//trim(str)//suf_bias, status='old', action='read', iostat=ios)
-      if (ios .ne. 0) then
-        write (6,*) 'CLOUDBRAIN keras matrices unable to load, abort.'
-        stop
-      endif
-      read(555,*) bias_int(n, :)
-      close (555)
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_kernel', n+1, pref//trim(str)//suf_kernel
-      endif
-      open (unit=555, file=pref//trim(str)//suf_kernel, status='old', action='read') 
-      do k=1,width
-        read(555,*) weights_int(n, k,:)
-      end do
-      close (555)
-#ifdef BRAINDEBUG
-        if (masterproc) then
-          write (6,*) 'BRAINDEBUG weights_* = ',n+1, weights_int(n, k,:)
-          write (6,*) 'BRAINDEBUG bias_* = ',n+1, bias_int(n, :)
-        endif
-#endif
-    end do
-
-    ! 3. Output layer
-    if (nn_nint+2 .lt. 10) then
-      write (str, '(I1)') nn_nint+2
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_bias = output', nn_nint+2, pref//trim(str)//suf_bias
-      endif
-      open (unit=555, file=pref//trim(str)//suf_bias, status='old', action='read', iostat=ios)
-      if (ios .ne. 0) then
-        write (6,*) 'CLOUDBRAIN keras matrices unable to load, abort.'
-        stop
-      endif
-      read(555,*) bias_out(:)
-      close (555)
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_kernel = output', nn_nint+2, pref//trim(str)//suf_kernel
-      endif
-      open (unit=555, file=pref//trim(str)//suf_kernel, status='old', action='read')
-    else
-      write (str10, '(I2)') nn_nint+2
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_bias = output', nn_nint+2, pref//trim(str10)//suf_bias
-      endif
-      open (unit=555, file=pref//trim(str10)//suf_bias, status='old', action='read', iostat=ios)
-      if (ios .ne. 0) then
-        write (6,*) 'CLOUDBRAIN keras matrices unable to load, abort.'
-        stop
-      endif
-      read(555,*) bias_out(:)
-      close (555)
-      if (masterproc) then
-        write (6,*) 'CLOUDBRAIN: reading layer*_kernel = output', nn_nint+2, pref//trim(str10)//suf_kernel
-      endif
-      open (unit=555, file=pref//trim(str10)//suf_kernel, status='old', action='read')
-    endif
-    do k=1,outputlength
-      read(555,*) weights_out(k,:)
-    end do
-    close (555)
-#ifdef BRAINDEBUG
-      if (masterproc) then
-        write (6,*) 'BRAINDEBUG weights_out = ',weights_out
-        write (6,*) 'BRAINDEBUG bias_out = ',bias_out
-      endif
-#endif
-#endif
-
+! TODO: Construct the input causal 1/0 matrix (noutput)
   end subroutine init_keras_matrices
     
-
 subroutine init_keras_norm()
-
   ! 1. Read sub
   if (masterproc) then
     write (6,*) 'CLOUDBRAIN: reading inp_sub'
   endif
-  open (unit=555,file='./keras_matrices/inp_sub.txt',status='old',action='read')
+  open(unit=555,file='/home1/08098/tg873976/usmile/causality_convection/dummy_singleNNs_FKB_renamed/0_24_inp_sub.txt',status='old',action='read')
   read(555,*) inp_sub(:)
   close (555)
 #ifdef BRAINDEBUG
@@ -371,7 +175,7 @@ subroutine init_keras_norm()
   if (masterproc) then
     write (6,*) 'CLOUDBRAIN: reading inp_div'
   endif
-  open (unit=555,file='./keras_matrices/inp_div.txt',status='old',action='read')
+  open(unit=555,file='/home1/08098/tg873976/usmile/causality_convection/dummy_singleNNs_FKB_renamed/0_24_inp_div.txt',status='old',action='read')
   read(555,*) inp_div(:)
   close (555)
 #ifdef BRAINDEBUG
@@ -380,13 +184,15 @@ subroutine init_keras_norm()
     endif
 #endif
 
+  ! HEY still need to think about output scaling.
   ! 3. Read out_scale
   if (masterproc) then
     write (6,*) 'CLOUDBRAIN: reading out_scale'
   endif
-  open (unit=555,file='./keras_matrices/out_scale.txt',status='old',action='read')
-  read(555,*) out_scale(:)
-  close (555)
+!  open (unit=555,file='./keras_matrices/out_scale.txt',status='old',action='read')
+!  read(555,*) out_scale(:)
+!  close (555)
+   out_scale(:) = 1.
 #ifdef BRAINDEBUG
     if (masterproc) then
       write (6,*) 'BRAINDEBUG out_scale = ',out_scale

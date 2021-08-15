7a2f3a38160e1d69b42c058f184e0f4fb93c8bb1
Jerry Lin <jerryl9@uci.edu>
2021-08-14 21:09:01 -0500

    added functionality for automated runs of any given model across spaced out initial conditions

---
+++ scripts/biasTrainingDataFactory/multirun.py
@@ -0,0 +1,23 @@
+import os
+import sys
+icPath = "$SCRATCH/icSpawn32/run/"
+icFiles = os.popen(" ".join(["ls", icPath + "And*.i.*.nc"])).read().splitlines()
+
+
+# THIS FILE REQUIRES YOU TO INPUT TWO NUMBERS. 
+# 1.) THE MODEL NUMBER YOU WISH TO USE 
+# 2.) THE FREQUENCY OF INITIAL CONDITION FILES USED. (ie every 7th, 8th, etc)
+
+# PICK A MODEL!
+modelNum = int(sys.argv[1])
+modLabel = "%05d" %modelNum
+rundir = "$SCRATCH/biasTrainingData/run_" + modLabel
+os.system(" ".join(["mkdir", "-p", rundir]))
+os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
+modelNum = str(modelNum)
+
+#PICK A FREQUENCY
+everyWhich = int(sys.argv[2])
+for i in range(len(icFiles)):
+    if (i+1)%everyWhich==0: 
+        os.system(" ".join(["python", "singlerun.py", modelNum, icFiles[i]]))
---
+++ scripts/biasTrainingDataFactory/singlerun.py
@@ -0,0 +1,43 @@
+import os
+import fileinput
+import sys
+pathOttmodels = "/work/08110/tg874091/stampede2/all_models_fkb_ott_et_al/"
+pathICs = "/scratch/08110/tg874091/icSpawn32/run/"
+
+# PICK A MODEL NUMBER!
+modelNum = int(sys.argv[1])
+modLabel = "%05d" %modelNum
+rundir = "$SCRATCH/biasTrainingData/run_" + modLabel
+
+#This should be commented out if using multi run as this is done in that python script.
+#os.system(" ".join(["mkdir", "-p", rundir]))
+#os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
+
+
+# PICK AN INITIAL CONDITION FILE!
+initFile = sys.argv[2]
+
+#Copying in Jordan's NN to overwrite keras_matrices/model.txt
+os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
+#Copying in initial condition file
+os.system(" ".join(["cp", pathICs + initFile, rundir]))
+#Making a custom atm_in file
+os.system(" ".join(["cp", "atm_in.template", "atm_in"]))
+with fileinput.FileInput("atm_in", inplace=True) as file:
+    for line in file:
+        print(line.replace("XXX", modLabel), end='')
+with fileinput.FileInput("atm_in", inplace=True) as file:
+    for line in file:
+        print(line.replace("initialConditionFileName", initFile), end='')
+os.system(" ".join(["mv", "atm_in", rundir]))
+#Making a custom run file
+os.system(" ".join(["cp", "run.template", "run.csh"]))
+with fileinput.FileInput("run.csh", inplace=True) as file:
+    for line in file:
+        print(line.replace("RUNDIR", rundir), end='')
+with fileinput.FileInput("run.csh", inplace=True) as file:
+    for line in file:
+        print(line.replace("XXX", modLabel), end='')
+os.system(" ".join(["mv", "run.csh", rundir]))
+#Running the model
+os.system(" ".join(["sbatch", rundir + "/run.csh"]))

67c4ebe448ae1f94bb1c406d2d0f7a8627fdf9df
Jerry Lin <jerryl9@uci.edu>
2021-08-14 21:31:27 -0500

    made some refinements to the multi run code

--- scripts/biasTrainingDataFactory/multirun.py
+++ scripts/biasTrainingDataFactory/multirun.py
@@ -1,5 +1,6 @@
 import os
 import sys
+pathOttmodels = "/work/08110/tg874091/stampede2/all_models_fkb_ott_et_al/"
 icPath = "$SCRATCH/icSpawn32/run/"
 icFiles = os.popen(" ".join(["ls", icPath + "And*.i.*.nc"])).read().splitlines()
 
@@ -11,13 +12,24 @@ icFiles = os.popen(" ".join(["ls", icPath + "And*.i.*.nc"])).read().splitlines()
 # PICK A MODEL!
 modelNum = int(sys.argv[1])
 modLabel = "%05d" %modelNum
-rundir = "$SCRATCH/biasTrainingData/run_" + modLabel
+rundir = "$SCRATCH/biasTrainingData/run_" + modLabel + "/"
 os.system(" ".join(["mkdir", "-p", rundir]))
 os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
 modelNum = str(modelNum)
+os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
+#Making a custom run file
+os.system(" ".join(["cp", "run.template", "run.csh"]))
+with fileinput.FileInput("run.csh", inplace=True) as file:
+    for line in file:
+        print(line.replace("RUNDIR", rundir), end='')
+with fileinput.FileInput("run.csh", inplace=True) as file:
+    for line in file:
+        print(line.replace("XXX", modLabel), end='')
+os.system(" ".join(["mv", "run.csh", rundir]))
 
 #PICK A FREQUENCY
 everyWhich = int(sys.argv[2])
 for i in range(len(icFiles)):
     if (i+1)%everyWhich==0: 
         os.system(" ".join(["python", "singlerun.py", modelNum, icFiles[i]]))
+        os.system(" ".join(["rm", rundir, "atm_in"]))
--- scripts/biasTrainingDataFactory/singlerun.py
+++ scripts/biasTrainingDataFactory/singlerun.py
@@ -18,7 +18,7 @@ rundir = "$SCRATCH/biasTrainingData/run_" + modLabel
 initFile = sys.argv[2]
 
 #Copying in Jordan's NN to overwrite keras_matrices/model.txt
-os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
+#os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
 #Copying in initial condition file
 os.system(" ".join(["cp", pathICs + initFile, rundir]))
 #Making a custom atm_in file
@@ -31,13 +31,15 @@ with fileinput.FileInput("atm_in", inplace=True) as file:
         print(line.replace("initialConditionFileName", initFile), end='')
 os.system(" ".join(["mv", "atm_in", rundir]))
 #Making a custom run file
-os.system(" ".join(["cp", "run.template", "run.csh"]))
-with fileinput.FileInput("run.csh", inplace=True) as file:
-    for line in file:
-        print(line.replace("RUNDIR", rundir), end='')
-with fileinput.FileInput("run.csh", inplace=True) as file:
-    for line in file:
-        print(line.replace("XXX", modLabel), end='')
-os.system(" ".join(["mv", "run.csh", rundir]))
-#Running the model
+#This code is not necessary when using the multirun.py script
+#os.system(" ".join(["cp", "run.template", "run.csh"]))
+#with fileinput.FileInput("run.csh", inplace=True) as file:
+#    for line in file:
+#        print(line.replace("RUNDIR", rundir), end='')
+#with fileinput.FileInput("run.csh", inplace=True) as file:
+#    for line in file:
+#        print(line.replace("XXX", modLabel), end='')
+#os.system(" ".join(["mv", "run.csh", rundir]))
+
+#RUNNING THE MODEL
 os.system(" ".join(["sbatch", rundir + "/run.csh"]))

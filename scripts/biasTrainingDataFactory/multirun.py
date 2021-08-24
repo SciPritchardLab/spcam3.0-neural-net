cc88b1542dcc660c9574c96d4e46e50f8abeb6d6
Jerry Lin <jerryl9@uci.edu>
2021-08-24 00:05:11 -0500

    Output is in separate subdirectories for each initial condition for each run

--- scripts/biasTrainingDataFactory/multirun.py
+++ scripts/biasTrainingDataFactory/multirun.py
@@ -5,26 +5,16 @@ pathOttmodels = "/work/08110/tg874091/stampede2/all_models_fkb_ott_et_al/"
 icPath = "$SCRATCH/icSpawn32/run/"
 icFiles = os.popen(" ".join(["ls", icPath + "And*.i.*.nc"])).read().splitlines()
 
-
-# THIS FILE REQUIRES YOU TO INPUT TWO NUMBERS. 
+# THIS SCRIPT REQUIRES TWO INPUTS. 
 # 1.) THE MODEL NUMBER YOU WISH TO USE 
 # 2.) THE FREQUENCY OF INITIAL CONDITION FILES USED. (ie every 7th, 8th, etc)
 
 # PICK A MODEL!
 modelNum = int(sys.argv[1])
-modLabel = "%05d" %modelNum
-rundir = "$SCRATCH/biasTrainingData/run_" + modLabel + "/"
-os.system(" ".join(["mkdir", "-p", rundir]))
-os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
 modelNum = str(modelNum)
 
-#Copying in Jordan's NN to overwrite keras_matrices/model.txt
-os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
-
-#PICK A FREQUENCY
+#PICK A FREQUENCY!
 everyWhich = int(sys.argv[2])
 for i in range(len(icFiles)):
     if (i+1)%everyWhich==0: 
         os.system(" ".join(["python", "singlerun.py", modelNum, icFiles[i]]))
-        os.system(" ".join(["rm", rundir + "atm_in"]))
-        os.system(" ".join(["rm", rundir + "run.csh"]))
--- scripts/biasTrainingDataFactory/singlerun.py
+++ scripts/biasTrainingDataFactory/singlerun.py
@@ -5,28 +5,34 @@ import re
 pathOttmodels = "/work/08110/tg874091/stampede2/all_models_fkb_ott_et_al/"
 pathICs = "/scratch/08110/tg874091/icSpawn32/run/"
 
+#THIS FILE REQUIRES TWO INPUTS. 
+# 1.) THE MODEL NUMBER YOU WISH TO USE 
+# 2.) THE NAME OF THE INITIAL CONDITION FILE YOU'D LIKE TO USE
+
 # PICK A MODEL NUMBER!
 modelNum = int(sys.argv[1])
 modLabel = "%05d" %modelNum
-rundir = "$SCRATCH/biasTrainingData/run_" + modLabel
-
-#This should be commented out if using multi run as this is done in that python script.
-#os.system(" ".join(["mkdir", "-p", rundir]))
-#os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
-
 
 # PICK AN INITIAL CONDITION FILE!
 initFile = sys.argv[2]
-# making nickname for job (unclear as to whether or not there are restrictions on job name. Will ask later.
+# making nickname for job
 nickname = re.sub("^[^-]+", "", initFile)
 nickname = re.sub("[^-]+$", "", nickname)
-nickname = re.sub("-", "", nickname)
-nickname = str(modelNum) + nickname
+nickname = re.sub("-$", "", nickname)
+nickname = re.sub("-", "_", nickname)
+nickname = modLabel + nickname
+
+rundir = "$SCRATCH/biasTrainingData/run_" + modLabel + "/" + nickname 
+
+os.system(" ".join(["mkdir", "-p", rundir]))
+os.system(" ".join(["cp", "-r", "baseline/*", rundir]))
 
 #Copying in Jordan's NN to overwrite keras_matrices/model.txt
-#os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
-#Copying in initial condition file + print path/name as a sanity check
+os.system(" ".join(["cp", pathOttmodels + modLabel + ".txt", rundir + "/keras_matrices/model.txt"]))
+
+#Copying in initial condition file
 os.system(" ".join(["cp", initFile, rundir]))
+
 #Making a custom atm_in file
 os.system(" ".join(["cp", "atm_in.template", "atm_in"]))
 with fileinput.FileInput("atm_in", inplace=True) as file:
@@ -36,6 +42,8 @@ with fileinput.FileInput("atm_in", inplace=True) as file:
     for line in file:
         print(line.replace("initialConditionFileName", initFile), end='')
 os.system(" ".join(["mv", "atm_in", rundir]))
+os.system(" ".join(["rm", "atm_in"]))
+
 #Making a custom run file
 os.system(" ".join(["cp", "run.template", "run.csh"]))
 with fileinput.FileInput("run.csh", inplace=True) as file:
@@ -45,6 +53,7 @@ with fileinput.FileInput("run.csh", inplace=True) as file:
     for line in file:
         print(line.replace("INITCOND", nickname), end='')
 os.system(" ".join(["mv", "run.csh", rundir]))
+os.system(" ".join(["rm", "run.csh"]))
 
 #RUNNING THE MODEL
 os.system(" ".join(["sbatch", rundir + "/run.csh"]))

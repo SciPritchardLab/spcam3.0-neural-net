a3d7e6967f80c585c0b7bc61db0b778793214ff1
Jerry Lin <jerryl9@uci.edu>
2022-06-17 11:58:15 -0500

    adding code for ensemble testing for different initial conditions (for NN or SP)

---
+++ scripts/ic_NN_ensembleTesting/baseline/obj/params.h
@@ -0,0 +1,15 @@
+#ifndef PARAMS_SET
+#define PARAMS_SET
+#define PLON  128
+#define PLAT  64
+#define PLEV  30
+#define PLEVR 30
+#define PCNST 3
+#define PNATS 0
+#define PTRM  63
+#define PTRN  63
+#define PTRK  63
+#define PCOLS 8
+#define CRM
+#undef STAGGERED
+#endif
---
+++ scripts/ic_NN_ensembleTesting/multirun.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import fileinput
+pathOttmodels = "/work/08110/tg874091/stampede2/all_models_fkb_ott_et_al/"
+icPath = "$SCRATCH/icSpawn32/run/"
+icFiles = os.popen(" ".join(["ls", icPath + "And*.i.*.nc"])).read().splitlines()
+
+# THIS SCRIPT REQUIRES TWO INPUTS. 
+# 1.) THE MODEL NUMBER YOU WISH TO USE 
+# 2.) THE FREQUENCY OF INITIAL CONDITION FILES USED. (ie every 7th, 8th, etc)
+
+# PICK A MODEL!
+modelNum = int(sys.argv[1])
+modelNum = str(modelNum)
+
+#PICK A FREQUENCY!
+everyWhich = int(sys.argv[2])
+for i in range(len(icFiles)):
+    if (i+1)%everyWhich==0: 
+        os.system(" ".join(["python", "singlerun.py", modelNum, icFiles[i]]))

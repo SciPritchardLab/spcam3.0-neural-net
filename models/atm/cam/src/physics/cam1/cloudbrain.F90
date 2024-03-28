53183c4124d1608dc345da9ae7999c341aa5a454
Jerry Lin <jlin96@br014.ib.bridges2.psc.edu>
2024-03-28 18:44:19 -0400

    added preliminary ablation code

--- models/atm/cam/src/physics/cam1/cloudbrain.F90
+++ models/atm/cam/src/physics/cam1/cloudbrain.F90
@@ -117,7 +117,19 @@ use mod_ensemble, only: ensemble_type
         input((2*nlev+5):(2*nlev+5+nlev-1)) = nn_in%vbp(:nlev)
         input((2*nlev+5+nlev):2*nlev+5+2*nlev-1) = nn_in%o3vmr(:nlev)
         input(2*nlev+5+2*nlev) = nn_in%coszrs
-      case('IN_TBP_QBP_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ_ABLATED')
+      case('IN_TBP_QBP_TPHYSTND_PHQ_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ')
+        input(1:nlev) = nn_in%tbp(:nlev)
+        input((1*nlev+1):2*nlev) = nn_in%qbp(:nlev)
+        input((2*nlev+1):3*nlev) = nn_in%dtdtm1(:nlev)
+        input((3*nlev+1):4*nlev) = nn_in%dqdtm1(:nlev)
+        input(4*nlev+1) = nn_in%ps
+        input(4*nlev+2) = nn_in%solin
+        input(4*nlev+3) = nn_in%shf
+        input(4*nlev+4) = nn_in%lhf
+        input((4*nlev+5):(4*nlev+5+nlev-1)) = nn_in%vbp(:nlev)
+        input((4*nlev+5+nlev):4*nlev+5+2*nlev-1) = nn_in%o3vmr(:nlev)
+        input(4*nlev+5+2*nlev) = nn_in%coszrs
+      case('IN_TBP_QBP_TPHYSTND_PHQ_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ_ABLATED')
         input(1:nlev) = nn_in%tbp(:nlev)
         input(nlev+1) = 0
         input(nlev+2) = 0
@@ -125,13 +137,13 @@ use mod_ensemble, only: ensemble_type
         input(nlev+4) = 0
         input(nlev+5) = 0
         input((nlev+6):2*nlev-5) = nn_in%qbp(:nlev)
-        input(2*nlev+1) = nn_in%ps
-        input(2*nlev+2) = nn_in%solin
-        input(2*nlev+3) = nn_in%shf
-        input(2*nlev+4) = nn_in%lhf
-        input((2*nlev+5):(2*nlev+5+nlev-1)) = nn_in%vbp(:nlev)
-        input((2*nlev+5+nlev):2*nlev+5+2*nlev-1) = nn_in%o3vmr(:nlev)
-        input(2*nlev+5+2*nlev) = nn_in%coszrs
+        input(2*nlev+1-5) = nn_in%ps
+        input(2*nlev+2-5) = nn_in%solin
+        input(2*nlev+3-5) = nn_in%shf
+        input(2*nlev+4-5) = nn_in%lhf
+        input((2*nlev+5-5):(2*nlev+5-5+nlev-1)) = nn_in%vbp(:nlev)
+        input((2*nlev+5-5+nlev):2*nlev+5-5+2*nlev-1) = nn_in%o3vmr(:nlev)
+        input(2*nlev+5-5+2*nlev) = nn_in%coszrs
     end select
 
 #ifdef BRAINDEBUG
@@ -187,19 +199,17 @@ use mod_ensemble, only: ensemble_type
       case('IN_TBP_QBP_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ')
         nn_out%tphystnd(:nlev) = output(1:nlev)
         nn_out%phq(:nlev) = output((nlev+1):2*nlev) ! This is still the wrong unit, needs to be converted to W/m^2
-      case('IN_TBP_QBP_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ_ABLATED')
-        nn_out%tphystnd(1) = 0
-        nn_out%tphystnd(2) = 0
-        nn_out%tphystnd(3) = 0
-        nn_out%tphystnd(4) = 0
-        nn_out%tphystnd(5) = 0
-        nn_out%tphystnd(:nlev) = output(6:nlev)
+      case('IN_TBP_QBP_TPHYSTND_PHQ_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ')
+        nn_out%tphystnd(:nlev) = output(1:nlev)
+        nn_out%phq(:nlev) = output((nlev+1):2*nlev) ! This is still the wrong unit, needs to be converted to W/m^2
+      case('IN_TBP_QBP_TPHYSTND_PHQ_PS_SOLIN_SHF_LHF_VBP_O3VMR_COSZRS_OUT_TPHYSTND_PHQ_ABLATED')
+        nn_out%tphystnd(:nlev) = output(1:nlev)
         nn_out%phq(1) = 0
         nn_out%phq(2) = 0
         nn_out%phq(3) = 0
         nn_out%phq(4) = 0
         nn_out%phq(5) = 0
-        nn_out%phq(:nlev) = output((nlev+1):5+2*nlev) ! This is still the wrong unit, needs to be converted to W/m^2     
+        nn_out%phq(nlev+1+5:nlev) = output((nlev+1):2*nlev-5) ! This is still the wrong unit, needs to be converted to W/m^2     
     end select
 
   end subroutine neural_net
